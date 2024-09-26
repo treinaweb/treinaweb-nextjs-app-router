bb14bbd800e2e8c0e61121c85ed097838ed19dc4
Wesley Gado <wesleygado@gmail.com>
2024-09-26 17:34:36 -0300

    Rotas dinamicas

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,16 @@
+export default function BlogPost({ params }: { params: { slug: string} }) {
+  const posts = [
+    { slug: 'primeiro-post-dinamico', title: "Primeiro Post Dinamico", content: 'Lorem 1 ipsum Lorem Loremdolor sit amet' },
+    { slug: 'segundo-post-dinamico', title: "Segundo Post Dinamico", content: 'Lorem 2 ipsum Lorem dolor sit ametLorem ' },
+    { slug: 'terceiro-post-dinamico', title: "Terceiro Post Dinamico", content: 'Lorem 3 ipsum dolor sit amet Lorem Lorem' },
+  ]
+  const post = posts.find(post => post.slug === params.slug)
+
+  return (
+    <div style={{textAlign: 'center', padding: '20px'}}>
+      <h1>{post?.title}</h1>
+      <br />
+      <p>{post?.content}</p>
+    </div>
+  )
+}
---
+++ src/app/blog/page.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link"
+
+export default function BlogHome() {
+  const posts = [
+    { id: '1', title: "Primeiro Post Dinamico", slug: 'primeiro-post-dinamico'},
+    { id: '2', title: "Segundo Post Dinamico", slug: 'segundo-post-dinamico'},
+    { id: '3', title: "Terceiro Post Dinamico", slug: 'terceiro-post-dinamico'},
+  ]
+
+  return (
+    <div>
+      <h1>Blog</h1>
+      {posts.map((post) => (
+        <li key={post.id}>
+          <Link href={`/blog/${post.slug}`}>{post.slug}</Link>
+        </li>
+      ))}
+    </div>
+  )
+}
+
